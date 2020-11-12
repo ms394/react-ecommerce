caa0f48618e2bd61ac40fc5f3905941f6cd6811a
ms394 <mohsinsajan394@gmail.com>
2020-11-12 23:02:33 +0530

    collection component with styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 import { Switch, Route } from "react-router-dom";
 
-const Hatspage = () => {
-  return <h1>HATS PAGE</h1>;
-};
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop/hats" component={Hatspage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./shop.css";
+import SHOP_DATA from "./shopData.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+
+class ShopPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
