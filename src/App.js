489edac2c12c6b94956cdbbfe07aced6f5455d05
ms394 <mohsinsajan394@gmail.com>
2020-11-12 18:46:00 +0530

    added Routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
+import { Switch, Route } from "react-router-dom";
+
+const Hatspage = () => {
+  return <h1>HATS PAGE</h1>;
+};
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop/hats" component={Hatspage} />
+      </Switch>
     </div>
   );
 }
